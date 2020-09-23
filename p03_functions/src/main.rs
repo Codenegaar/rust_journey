fa13cc08868de3f2ccf2ce10e5b26c6d224698bd
Ali Rashidi <alirashidi127@gmail.com>
2020-09-23 14:52:58 +0330

    Added a struct and basic method to p03

--- p03_functions/src/main.rs
+++ p03_functions/src/main.rs
@@ -1,7 +1,23 @@
+struct Student {
+    age: u16,
+    gender: char
+}
+impl Student {
+    fn say_my_name(&self) {
+        println!("My name is still unknown, but I'm {}", self.age);
+    }
+}
+
 fn main() {
     let a: u64 = 6;
     let f_a = fact(a);
     println!("Fact. of 6 is: {}", f_a);
+
+    let ali = Student {
+        age: 21,
+        gender: 'm'
+    };
+    ali.say_my_name();
 }
 
 fn fact(n: u64) -> u64 {
