6bc84e48801fc4495e7b7dfcf74bb90cebf23759
Ali Rashidi <alirashidi127@gmail.com>
2020-09-27 11:19:22 +0330

    Added documentation blocks

--- p05_cargo_pg/codar/src/math/advanced.rs
+++ p05_cargo_pg/codar/src/math/advanced.rs
@@ -1,3 +1,13 @@
+/// Raises the parameter `b` to the power of `p`
+///
+/// # Examples
+/// ```
+/// use codar::math::advanced::power;
+/// let b: i64 = 8;
+/// let p: i64 = 2;
+/// let res    = power(b, p);
+/// assert_eq!(res, 64);
+/// ```
 pub fn power(b: i64, p: i64) -> i64 {
     let mut result: i64 = 1;
     for _n in 0..p {
--- p05_cargo_pg/codar/src/math/simple.rs
+++ p05_cargo_pg/codar/src/math/simple.rs
@@ -1,3 +1,11 @@
+/// Adds a list of numbers and returns the result
+/// # Examples
+/// ```
+/// use codar::math::simple::add;
+/// let nums = vec![12, 14, 16];
+/// let res  = add(&nums);
+/// assert_eq!(res, 40);
+/// ```
 pub fn add(args: &[i64]) -> i64 {
     let mut result = 0;
     for arg in args.iter() {
