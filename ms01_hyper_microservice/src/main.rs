5abbd98f6f50bbfed14a132ede84a0491d823edc
Ali Rashidi <alirashidi127@gmail.com>
2020-10-07 22:44:28 +0330

    Hello world server

--- ms01_hyper_microservice/src/main.rs
+++ ms01_hyper_microservice/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn};
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+
+    //Create a service
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("Error starting the server: {}", e);
+    }
+}
+
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(
+        Response::new("Hello world".into())
+    )
 }
